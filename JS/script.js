e77fed3b3b18b3dbfd25ae4df099379d5537e2ed
Ebenezer Odame Aniagyei <99404199+MrEben@users.noreply.github.com>
2022-11-26 02:35:37 +0000

    added products,features page

--- JS/script.js
+++ JS/script.js
@@ -10,6 +10,29 @@ const navSlide = () => {
 };
 navSlide();
 
+// const updateFeaturesHeight = () => {
+
+// };
+// window.addEventListener('resize', updateFeaturesHeight());
+const updateFeaturesHeight = () => {
+  const featuresHeight = document.querySelector('.features');
+  const productsHeight = document.querySelector('.products');
+  const featureQualitiesHeight = document.querySelector('.features-qualities');
+  const productsDisplayHeight = document.querySelector('.products-display');
+  const qHeight = featureQualitiesHeight.getBoundingClientRect().height;
+  const pHeight = productsDisplayHeight.getBoundingClientRect().height;
+  console.log(qHeight);
+  const mQuery = window.matchMedia('(max-width:620px)');
+  const handleResize = (e) => {
+    if (e.matches) {
+      featuresHeight.style.height = `${qHeight * 5}px`;
+      productsHeight.style.height = `${pHeight * 5}px`;
+    }
+  };
+  mQuery.addListener(handleResize);
+};
+updateFeaturesHeight();
+
 const features = [
   {
     id: 1,
@@ -36,7 +59,34 @@ const features = [
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, non.',
   },
 ];
+const products = [
+  {
+    id: 1,
+    img: './images/download(1).jpg',
+    prodName: 'Bic',
+    price: 23.5,
+  },
+  {
+    id: 2,
+    img: './images/img4.jpg',
+    prodName: 'Befa',
+    price: 23.5,
+  },
+  {
+    id: 3,
+    img: './images/download(2).jpg',
+    prodName: 'Silk Blue',
+    price: 23.5,
+  },
+  {
+    id: 4,
+    img: './images/download.jpg',
+    prodName: 'Binf',
+    price: 23.5,
+  },
+];
 const feature = document.querySelector('.features-qualities');
+const product = document.querySelector('.products-display');
 window.addEventListener('DOMContentLoaded', () => {
   let displayFeatures = features.map(function (item) {
     const { id, img, quality, text } = item;
@@ -52,4 +102,17 @@ window.addEventListener('DOMContentLoaded', () => {
   });
   displayFeatures = displayFeatures.join('');
   feature.innerHTML = displayFeatures;
+  let displayProducts = products.map(function (item) {
+    const { id, img, prodName, price } = item;
+    return `<div class="each-product">
+              <div class="product-image">
+                <img src=${img} alt="image here" />
+              </div>
+              <h3>${prodName}</h3>
+              <h2>$${price}</h2>
+              <button class="btn btn-primary">purchase</button>
+            </div>`;
+  });
+  displayProducts = displayProducts.join('');
+  product.innerHTML = displayProducts;
 });
